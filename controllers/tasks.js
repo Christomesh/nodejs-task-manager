a741e7f0d38d40f056f9c01d1decf94ad094c1e2
Christomesh <meshacholuwadamilare@gmail.com>
2022-07-11 15:00:12 +0100

    updated ctreateTask and getAllTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,21 @@
+const Task = require('../models/Task')
 
-const getAllTasks = (req, res)=>{
-    res.json({"message":"get all tasks"})
+const getAllTasks = async (req, res)=>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({ tasks })
+    } catch (error) {
+        res.status(500).json({ msg:error })
+    }
 }
 
-const createTask = (req, res)=>{
-    res.json({"message":"create task"})
+const createTask = async (req, res)=>{
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 const getTask = (req, res)=>{
     res.json({"id":req.params.id})
