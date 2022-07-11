c0e2f8fa157c42243169e718112c9f8b944b44f6
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-11 08:27:56 +0100

    added gitignore

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+
+
+const port = 3000
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}...`)
+})
