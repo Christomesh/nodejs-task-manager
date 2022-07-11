0347e065ed0295a6bde9fd45f251af08650e681f
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-11 22:09:55 +0100

    Added updatedTask and deleteTask endpoint

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -17,15 +17,50 @@ const createTask = async (req, res)=>{
         res.status(500).json({msg:error})
     }
 }
-const getTask = (req, res)=>{
-    res.json({"id":req.params.id})
+const getTask = async (req, res)=>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id: taskID})
+
+        if (!task){
+            return res.status(404).json({msg:`No task with id: ${taskID}`})
+        }
+
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
-const updateTask = (req, res)=>{
-    res.json({"message":"update task"})
+const updateTask = async (req, res) => {
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
+            new:true,
+            runValidators: true,
+        })
+        if (!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
-const deleteTask = (req, res)=>{
-    res.json({"message":"deleted"})
+
+
+const deleteTask = async (req, res)=>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if (!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+
 }
 
 
