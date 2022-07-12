7c9b24efd30dbe9b640b89783868792069960133
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-12 04:56:22 +0100

    added custom error API

---
+++ error/custom-error.js
@@ -0,0 +1,16 @@
+
+class CustomErrorAPI extends Error{
+    constructor(message, statusCode){
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+const createCustomError = (msg, statusCode) =>{
+    return new CustomErrorAPI(msg, statusCode)
+}
+
+module.exports = {
+    createCustomError,
+    CustomErrorAPI
+}
